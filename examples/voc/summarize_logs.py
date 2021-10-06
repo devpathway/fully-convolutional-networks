0e1586e8ffadc863a8d102625159cfed4dd767c2
devpathway <williamnation0302@gmail.com>
2021-10-06 20:49:12 +0100

    6.4.20

---
+++ examples/voc/summarize_logs.py
@@ -0,0 +1,142 @@
+#!/usr/bin/env python
+
+import datetime
+import json
+import os
+import os.path as osp
+import yaml
+
+import dateutil.parser
+import pandas
+import tabulate
+
+
+def summarize_log(logs_dir, name, params_keys, target_key, objective):
+    try:
+        params = yaml.load(open(osp.join(logs_dir, name, 'params.yaml')))
+    except Exception:
+        return None, None, osp.join(logs_dir, name)
+
+    log_file = osp.join(logs_dir, name, 'log')
+    try:
+        df = pandas.DataFrame(json.load(open(log_file)))
+    except Exception:
+        return None, None, osp.join(logs_dir, name)
+
+    try:
+        if objective == 'min':
+            idx = df[target_key].idxmin()
+        else:
+            idx = df[target_key].idxmax()
+    except Exception:
+        idx = None
+
+    dfi = df.ix[idx] if idx else None
+    row = []
+    is_active = True
+    for key in keys:
+        if key == 'name':
+            row.append(name)
+        elif key in ['epoch', 'iteration']:
+            if dfi is None:
+                value = '<none>'
+            else:
+                value = '%d' % dfi[key]
+            max_value = df[key].max()
+            row.append('%s /%d' % (value, max_value))
+        elif key == 'last_time':
+            if df is None:
+                value = '<none>'
+            else:
+                elapsed_time = df['elapsed_time'].max()
+                value = params.get('timestamp', '<none>')
+                if value != '<none>':
+                    value = dateutil.parser.parse(value)
+                    value += datetime.timedelta(seconds=elapsed_time)
+                    now = datetime.datetime.now()
+                    value = now - value
+                    if value > datetime.timedelta(minutes=10):
+                        is_active = False
+                    value -= datetime.timedelta(
+                        microseconds=value.microseconds)
+                    value = max(datetime.timedelta(seconds=0), value)
+                    value = '- %s' % value.__str__()
+            row.append(value)
+        elif key in params:
+            row.append(params[key])
+        elif dfi is not None and key in dfi:
+            if isinstance(dfi[key], float):
+                value = '%.3f' % dfi[key]
+                min_value = df[key].min()
+                max_value = df[key].max()
+                row.append('%.3f< %s <%.3f' % (min_value, value, max_value))
+            else:
+                row.append(dfi[key])
+        else:
+            row.append('<none>')
+    return row, is_active, None
+
+
+def _summarize_log(args):
+    return summarize_log(*args)
+
+
+def summarize_logs(logs_dir, keys, target_key, objective):
+    assert objective in ['min', 'max']
+    assert target_key in keys
+
+    args_list = []
+    for name in os.listdir(logs_dir):
+        args_list.append((
+            logs_dir,
+            name,
+            keys,
+            target_key,
+            objective,
+        ))
+
+    from concurrent.futures import ProcessPoolExecutor
+    with ProcessPoolExecutor(max_workers=4) as executor:
+        results = executor.map(_summarize_log, args_list)
+
+    rows = []
+    ignored = []
+    for row, is_active, log_dir_ignored in results:
+        if log_dir_ignored:
+            ignored.append(log_dir_ignored)
+            continue
+        rows.append(row)
+
+    rows = sorted(rows, key=lambda x: x[0], reverse=True)
+    print(tabulate.tabulate(rows, headers=keys,
+                            floatfmt='.3f', tablefmt='grid',
+                            numalign='center', stralign='center',
+                            showindex=True, disable_numparse=True))
+
+    if not ignored:
+        return
+
+    print('Ignored logs:')
+    for log_dir in ignored:
+        print('  - %s' % log_dir)
+
+
+if __name__ == '__main__':
+    here = osp.dirname(osp.abspath(__file__))
+    logs_dir = osp.join(here, 'logs')
+    print('# logs_dir = %s' % logs_dir)
+
+    keys = [
+        'name',
+        'last_time',
+        # 'dataset',
+        # 'git_hash',
+        # 'hostname',
+        'model',
+        # 'freeze',
+        'epoch',
+        'iteration',
+        'validation/main/miou',
+    ]
+    summarize_logs(logs_dir, keys, target_key=keys[-1],
+                   objective='max')
---
+++ examples/voc/train_fcn16s.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+
+import argparse
+import datetime
+import os
+import os.path as osp
+
+os.environ['MPLBACKEND'] = 'Agg'  # NOQA
+
+import chainer
+import fcn
+
+from train_fcn32s import get_data
+from train_fcn32s import get_trainer
+
+
+here = osp.dirname(osp.abspath(__file__))
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-g', '--gpu', type=int, required=True, help='gpu id')
+    parser.add_argument(
+        '--fcn32s-file', default=fcn.models.FCN32s.pretrained_model,
+        help='pretrained model file of FCN32s')
+    args = parser.parse_args()
+
+    args.model = 'FCN16s'
+    args.lr = 1.0e-12
+    args.momentum = 0.99
+    args.weight_decay = 0.0005
+
+    args.max_iteration = 100000
+    args.interval_print = 20
+    args.interval_eval = 4000
+
+    now = datetime.datetime.now()
+    args.timestamp = now.isoformat()
+    args.out = osp.join(here, 'logs', now.strftime('%Y%m%d_%H%M%S'))
+
+    # data
+    class_names, iter_train, iter_valid, iter_valid_raw = get_data()
+    n_class = len(class_names)
+
+    # model
+    fcn32s = fcn.models.FCN32s()
+    chainer.serializers.load_npz(args.fcn32s_file, fcn32s)
+    model = fcn.models.FCN16s(n_class=n_class)
+    model.init_from_fcn32s(fcn32s)
+
+    if args.gpu >= 0:
+        chainer.cuda.get_device(args.gpu).use()
+        model.to_gpu()
+
+    # optimizer
+    optimizer = chainer.optimizers.MomentumSGD(
+        lr=args.lr, momentum=args.momentum)
+    optimizer.setup(model)
+    optimizer.add_hook(chainer.optimizer.WeightDecay(rate=args.weight_decay))
+    for p in model.params():
+        if p.name == 'b':
+            p.update_rule = chainer.optimizers.momentum_sgd.MomentumSGDRule(
+                lr=optimizer.lr * 2, momentum=0)
+    model.upscore2.disable_update()
+    model.upscore16.disable_update()
+
+    # trainer
+    trainer = get_trainer(optimizer, iter_train, iter_valid, iter_valid_raw,
+                          class_names, args)
+    trainer.run()
+
+
+if __name__ == '__main__':
+    main()
